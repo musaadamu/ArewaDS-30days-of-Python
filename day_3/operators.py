82853eae119f28b826312230a7fdd5a55fa56f37
Musa Adamu <msmajemusa4@gmail.com>
2023-11-29 01:37:02 +0100

    day 3 began

---
+++ day_3/operators.py
@@ -0,0 +1,31 @@
+# 1. age declaration
+age = 30
+
+# 2. height as float variable
+height = 17.5
+
+# 3. a variable that stores a complex number
+num_complex = 3 + 34j
+
+# 4. base and height of triangle to calculate its area
+b = int(input('Enter the base of the triangle: '))
+h = int(input('Enter the height of the triangle: '))
+area = 0.5 * b * h
+
+# 5. Enter side a, side b and side c of a triangle
+a = int(input('Enter side a of the triangle: '))
+b = int(input('Enter side b of the triangle: '))
+c = int(input('Enter side c of the triangle: '))
+perimeter = a + b + c
+
+# 6. Getting the width and length of a rectangle to calculate it area
+length = int(input('Enter the length of the rectangle: '))
+width = int(input('Enter the width of the rectangle: '))
+area = length * width
+
+# 7. Getting the radius of a circlce using prompt
+r = int(input('Enter the radius of a circle'))
+pi = 3.14
+area = pi * r * r
+circumference = 2 * pi * r
+# 8. 
